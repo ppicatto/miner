3b47a1fc5291dd73f5204b76bbac2f2502bdd7ec
Pablo Gabriel Picatto <ppicatto@nubity.com>
2016-09-19 01:19:27 -0300

    [WIP] Changed command to service

--- src/TwitterBundle/Command/ImportCommand.php
+++ src/TwitterBundle/Command/ImportCommand.php
@@ -8,14 +8,11 @@
 
 namespace TwitterBundle\Command;
 
-use Goutte\Client;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DomCrawler\Crawler;
-use TwitterAPIExchange;
 
 /**
  * @author Hernan Andres Picatto <hpicatto@uscd.edu>
@@ -45,167 +42,11 @@ class ImportCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $dateFrom = new \DateTime($input->getOption('dateFrom'));
-        $dateTo = new \DateTime($input->getOption('dateTo'));
-        while ($dateFrom < $dateTo) {
-            $startAt = clone $dateFrom;
-            $dateFrom->modify('+1 day');
-            $output->writeln(sprintf('Importing from <info>%s</info> to <info>%s</info>', $startAt->format('Y-m-d'), $dateFrom->format('Y-m-d')));
-            foreach ($this->getTweets($input->getOption('hashtag'), $startAt->format('Y-m-d'), $dateFrom->format('Y-m-d'), $input->getOption('max')) as $page) {
-                $output->writeln(sprintf('Imported page <info>%s</info> with <info>%s</info> items', $page['number'], $page['items']));
-                if (isset($page['pageUrl'])) {
-                    $output->writeln(sprintf('<info>%s</info>', $page['pageUrl']));
-                }
-            }
+        $dateFrom = \DateTime::createFromFormat('Y-m-d', $input->getOption('dateFrom'));
+        $dateTo = \DateTime::createFromFormat('Y-m-d', $input->getOption('dateTo'));
+        foreach ($this->getContainer()->get('twitter_import_handler')->importTweets($dateFrom, $dateTo, $input->getOption('hashtag'), $input->getOption('max')) as $tweet) {
+//            dump($tweet);
         }
         $output->writeln('Process finished OK');
-    }
-
-    
-    private function getTweets(array $hashtag = [], $dateFrom, $dateTo, $max = 100)
-    {
-        $mongoClient = new \MongoClient();
-        $db = $mongoClient->selectDB("twitter"); 
-        $previousMetadata = $db->tweetMetadata->find()->sort(['createdAt'=>-1])->limit(1)->next();
-        $newTweets = [];
-        $hashtag = '%23' . implode('%28OR%28%23', $hashtag);
-        if (!$previousMetadata) {
-            $baseUrl = 'https://twitter.com/search?f=tweets&vertical=default&q='.$hashtag.'%20since%3A'.$dateFrom.'%20until%3A'.$dateTo.'%20include%3Aretweets&src=typd&count='.$max;
-
-            $client = new Client();
-            $crawler = $client->request('GET', $baseUrl);
-            $newTweets = $this->parseTweets($crawler);
-            $metadata = [
-                'firstTweet' => $newTweets[0]['tweetId'],
-                'lastTweet' => end($newTweets)['tweetId'],
-                'url' => $baseUrl,
-                'pageNumber' => 1,
-            ];
-            $db->tweetMetadata->insert($metadata);
-            $this->persistTweets($newTweets, $metadata);
-            yield [
-                'pageUrl' => $baseUrl,
-                'number' => 1,
-                'items' => count($newTweets),
-            ];
-            $pageNumber = 2;
-        } else {
-            $pageNumber = $previousMetadata['pageNumber'];
-        }
-        
-        while ($newTweets || $previousMetadata) {
-            if ($previousMetadata) {
-                $metadata = $previousMetadata;
-                $newTweets = $this->getNextPage($metadata['url']);
-                $previousMetadata = null;
-            } else {
-                $metadata = [
-                    'pageNumber' => $pageNumber,
-                    'url' => 'https://twitter.com/i/search/timeline?f=tweets&vertical=default&q='.$hashtag.'%20since%3A'.$dateFrom.'%20until%3A'.$dateTo.'%20include%3Aretweets&src=typd&include_available_features=1&include_entities=1&last_note_ts=3099&max_position=TWEET-'.$metadata['lastTweet'].'-'.$metadata['firstTweet'].'-BD1UO2FFu9QAAAAAAAAETAAAAAcAAAASAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA&reset_error_state=false',
-                ];
-                $newTweets = $this->getNextPage($metadata['url']);
-                if(!$newTweets) {
-                    break;
-                }
-                $metadata['firstTweet'] = $newTweets[0]['tweetId'];
-                $metadata['lastTweet'] = end($newTweets)['tweetId'];
-                $db->tweetMetadata->insert($metadata);
-            }
-            $this->persistTweets($newTweets, $metadata);
-            yield [
-                'pageUrl' => $metadata['url'],
-                'number' => $pageNumber,
-                'items' => count($newTweets),
-            ];
-            $pageNumber += 1;
-        }
-
-        yield [
-            'number' => $pageNumber,
-            'items' => count($newTweets)
-        ]; 
-    }
-
-    private function persistTweets($tweets, $metadata) {
-        $mongoClient = new \MongoClient();
-        $db = $mongoClient->selectDB("twitter"); 
-        foreach ($tweets as $tweet) {
-            if (!$db->tweet->findOne(['tweetId' => $tweet['tweetId']])) {
-                $tweet['metadataId'] = $metadata['_id'];
-                $db->tweet->insert($tweet);
-            }
-        }
-    }
-    
-    private function getNextPage($url) {
-        sleep(4);
-        $client = new Client();
-        $client->request('GET', $url);
-        $html = json_decode($client->getResponse()->getContent(), true)['items_html'];
-        $crawler = new Crawler($html);
-
-        return $this->parseTweets($crawler);
-        
-    }
-
-    /**
-     * @param string $twitterPlaceId
-     */
-    private function getCoordinates($twitterPlaceId) {
-        if (!$twitterPlaceId) {
-            return null;
-        }
-        $mongoClient = new \MongoClient();
-        $db = $mongoClient->selectDB("twitter");
-
-        if ($place = $db->places->findOne(['id' => $twitterPlaceId])) {
-
-            return $place['_id'];
-        }
-        $client = new Client();
-        $twitter = new TwitterAPIExchange($this->getContainer()->getParameter('twitter'));
-
-        try {
-            $response = json_decode($twitter
-                ->buildOauth(sprintf('https://api.twitter.com/1.1/geo/id/%s.json', $twitterPlaceId), 'GET')
-                ->performRequest()
-            );
-        } catch (\Exception $e) {
-            dump($e);
-        }
-        
-        if (isset($response->id)) {
-            $db->places->insert($response);
-
-            return $response->_id;
-        }
-        dump($response->errors[0]->message . sprintf(' waiting %s seconds', 60));
-        sleep(60);
-        $this->getCoordinates($twitterPlaceId);
-    }
-
-    /**
-     * @param Crawler $crawler
-     *
-     * @return array
-     */
-    private function parseTweets(Crawler $crawler)
-    {
-        return $crawler->filter('li.stream-item')->each(function ($node) {
-            $placeId = null;
-            $placeNode = $node->filter('.ProfileTweet-actionButton.u-linkClean.js-nav.js-geo-pivot-link');
-            if ($placeNode->count()) {
-                $placeId = $placeNode->attr('data-place-id');
-            }
-
-            return [
-                'screenname' => $node->filter('.fullname')->text(),
-                'username' => $node->filter('.username b')->text(),
-                'text' => $node->filter('.TweetTextSize.js-tweet-text.tweet-text')->text(),
-                'createdAt' => date('Y-m-d H:i:s', $node->filter('._timestamp.js-short-timestamp')->attr('data-time')),
-                'tweetId' => $node->attr('data-item-id'),
-                'place' => $this->getCoordinates($placeId)
-            ];
-        });
     }
 }
