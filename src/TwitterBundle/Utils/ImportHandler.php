ea0d731802af19b6c46bec9d20cf43ef5bfb7e5a
Pablo Gabriel Picatto <ppicatto@nubity.com>
2016-09-19 01:19:10 -0300

    [WIP] Changed command to service

---
+++ src/TwitterBundle/Utils/ImportHandler.php
@@ -0,0 +1,235 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace TwitterBundle\Utils;
+
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use TwitterAPIExchange;
+
+/**
+ * @author Hernan Andres Picatto <hpicatto@uscd.edu>
+ * @author Pablo Gabriel Picatto <p.picatto@gmail.com>
+ */
+class ImportHandler {
+
+    /**
+     * @var int 
+     */
+    private $page = 1;
+
+    /**
+     * @var string
+     */
+    private $firstId = null;
+
+    /**
+     * @var string
+     */
+    private $lastId = null;
+
+    /**
+     * @var \MongoDB 
+     */
+    private $db = null;
+
+    public function __construct() {
+        $mongoClient = new \MongoClient();
+        $this->db = $mongoClient->selectDB("twitter");
+    }
+
+    /**
+     * @param array $hashtag
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param int $max
+     *
+     * @return string
+     */
+    private function urlEncode(array $hashtag = [], \DateTime $dateFrom, \DateTime $dateTo, int $max = 200) {
+        return sprintf('%s&include:retweets&src=typd&count=%s', str_replace(
+            [':', '&', '#'],
+            ['%3A', '%20', '%23'],
+            sprintf(
+                'q=%s&since:%s&until:%s', 
+                '#' . implode('+OR+#', $hashtag),
+                $dateFrom->format('Y-m-d'),
+                $dateTo->format('Y-m-d')
+            )
+        ),$max);
+    }
+
+    /**
+     * @param array $hashtag
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param int $max
+     *
+     * @return array
+     */
+    private function getFirstPage(array $hashtag, \DateTime $dateFrom, \DateTime $dateTo, int $max = 200)
+    {
+        $client = new Client();
+
+        $crawler = $client->request('GET', sprintf('https://twitter.com/search?%s', $this->urlEncode($hashtag, $dateFrom, $dateTo, $max)));
+
+        return $this->parseTweets($crawler);
+    }
+
+    /**
+     * @param array $hashtag
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param int $max
+     *
+     * @return array
+     */
+    private function getNextPage(array $hashtag, \DateTime $dateFrom, \DateTime $dateTo, int $max)
+    {
+        sleep(4);
+        $client = new Client();
+        dump(sprintf(
+            'https://twitter.com/i/search/timeline?%s&f=tweets&vertical=default&include_available_features=1&include_entities=1&last_note_ts=3099&max_position=TWEET-%s-%s-BD1UO2FFu9QAAAAAAAAETAAAAAcAAAASAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA&reset_error_state=false',
+            $this->urlEncode($hashtag, $dateFrom, $dateTo, $max),
+            $this->lastId,
+            $this->firstId
+        ));
+        $client->request('GET', sprintf(
+            'https://twitter.com/i/search/timeline?%s&f=tweets&vertical=default&include_available_features=1&include_entities=1&last_note_ts=3099&max_position=TWEET-%s-%s-BD1UO2FFu9QAAAAAAAAETAAAAAcAAAASAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA&reset_error_state=false',
+            $this->urlEncode($hashtag, $dateFrom, $dateTo, $max),
+            $this->lastId,
+            $this->firstId
+        ));
+        $html = json_decode($client->getResponse()->getContent(), true)['items_html'];
+        $crawler = new Crawler($html);
+
+        return $this->parseTweets($crawler);
+    }
+
+    /**
+     * @param string $twitterPlaceId
+     */
+    private function getCoordinates($twitterPlaceId) {
+        if (!$twitterPlaceId) {
+            return null;
+        }
+
+        if ($place = $this->db->places->findOne(['id' => $twitterPlaceId])) {
+
+            return $place['_id'];
+        }
+        $client = new Client();
+        $twitter = new TwitterAPIExchange($this->getContainer()->getParameter('twitter'));
+
+        try {
+            $response = json_decode($twitter
+                ->buildOauth(sprintf('https://api.twitter.com/1.1/geo/id/%s.json', $twitterPlaceId), 'GET')
+                ->performRequest()
+            );
+        } catch (\Exception $e) {
+            dump($e);
+        }
+        
+        if (isset($response->id)) {
+            $this->db->places->insert($response);
+
+            return $response->_id;
+        }
+        dump($response->errors[0]->message . sprintf(' waiting %s seconds', 60));
+        sleep(60);
+        $this->getCoordinates($twitterPlaceId);
+    }
+
+    /**
+     * @param Crawler $crawler
+     *
+     * @return array
+     */
+    private function parseTweets(Crawler $crawler)
+    {
+        return $crawler->filter('li.stream-item')->each(function ($node) {
+            $placeId = null;
+            $placeNode = $node->filter('.ProfileTweet-actionButton.u-linkClean.js-nav.js-geo-pivot-link');
+            if ($placeNode->count()) {
+                $placeId = $placeNode->attr('data-place-id');
+            }
+
+            return [
+                'screenname' => $node->filter('.fullname')->text(),
+                'username' => $node->filter('.username b')->text(),
+                'text' => $node->filter('.TweetTextSize.js-tweet-text.tweet-text')->text(),
+                'createdAt' => date('Y-m-d H:i:s', $node->filter('._timestamp.js-short-timestamp')->attr('data-time')),
+                'tweetId' => $node->attr('data-item-id'),
+                'place' => $this->getCoordinates($placeId)
+            ];
+        });
+    }
+
+    /**
+     * @param \DateTime $dateFrom
+     * @param \DateTime $dateTo
+     * @param array $hashtag
+     * @param int $max
+     */
+    public function importTweets(\DateTime $dateFrom, \DateTime $dateTo, array $hashtag = [], int $max = 200)
+    {
+        if ($previousMetadata = $this->db->tweetMetadata->find()->sort(['createdAt' => -1])->limit(1)->next()) {
+            $this->page = $previousMetadata['page'];
+            $dateFrom = $previousMetadata['importedDate'];
+            $this->firstId = $previousMetadata['firstId'];
+            $this->lastId = $previousMetadata['lastId'];
+            $this->page++;
+        }
+
+        while ($dateFrom < $dateTo) {
+            $startAt = clone $dateFrom;
+            $dateFrom->modify('+1 day');
+            if (1 === $this->page) {
+                $tweets = $this->getFirstPage($hashtag, $startAt, $dateFrom, $max);
+            } else {
+                $tweets = $this->getNextPage($hashtag, $startAt, $dateFrom, $max);
+            }
+
+            if (!$tweets) {
+                $this->page = 1;
+                continue;
+            }
+            while ($tweets) {
+                $metadata = [
+                    'hashtags' => $hashtag,
+                    'dateFrom' => $dateFrom,
+                    'dateTo' => $dateTo,
+                    'importedDate' => $startAt,
+                    'limit' => $max,
+                    'page' => $this->page,
+                ];
+                yield from $this->persistData($tweets, $metadata);
+                $this->page++;
+                $tweets = $this->getNextPage($hashtag, $startAt, $dateFrom, $max);
+            }
+
+            $this->page = 1;
+        }
+    }
+
+    private function persistData($tweets, $metadata) {
+        $this->firstId = $tweets[0]['tweetId'];
+        $this->lastId = end($tweets)['tweetId'];
+        $metadata['firstId'] = $this->firstId;
+        $metadata['lastId'] = $this->lastId;
+        dump($metadata);
+        $this->db->tweetMetadata->insert($metadata);
+        foreach ($tweets as $tweet) {
+            if (!$this->db->tweet->findOne(['tweetId' => $tweet['tweetId']])) {
+                $tweet['metadataId'] = $metadata['_id'];
+                $this->db->tweet->insert($tweet);
+            }
+            yield $tweet;
+        }
+    }
+}
